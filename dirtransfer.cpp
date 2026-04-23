1a3abcf1f94a58e83f58685a8da0066c394d73a5
coflahertywsu <c.oflaherty@wsu.edu>
2026-04-22 21:38:23 -0700

    Improvements and extensions to dirTransfer class

--- dirtransfer.cpp
+++ dirtransfer.cpp
@@ -4,56 +4,132 @@ DirTransfer::DirTransfer(Network *network,QObject *parent)
     : QObject{parent}
 {
     m_network = network;
+    m_localRepo = QDir(LOCAL_REPO);
+
+    connect(&m_fileSystemWatcher, &QFileSystemWatcher::directoryChanged, this, &DirTransfer::onLocalRepoChanged);
 }
 
-void DirTransfer::setDir(const QDir &dir)
+void DirTransfer::setDirToCopy(const QDir &dir)
 {
-    m_localDir = dir;
-    m_remoteDir = QDir("//" + m_network->jetsonIp() + REMOTE_PARENT_DIR + "//"  + dir.dirName());
+    m_selectedDir = dir;
+    m_remoteDir = QDir("//" + m_network->jetsonIp() + "/" + REMOTE_REPO + "/"  + dir.dirName());
 }
 
-bool DirTransfer::remoteCopyExists()
+void DirTransfer::setLocalRepoWatcher(bool enable)
+{
+    if(enable)
+    {
+        if(!m_fileSystemWatcher.directories().contains(m_localRepo.path()))
+            m_fileSystemWatcher.addPath(m_localRepo.path());
+
+        onLocalRepoChanged();
+    }
+    else
+    {
+        m_fileSystemWatcher.removePath(m_localRepo.path());
+    }
+}
+
+bool DirTransfer::selectedDirIsValid() const
+{
+    QFileInfoList fileInfoList = m_selectedDir.entryInfoList(QDir::Files | QDir::NoDotAndDotDot);
+
+    bool modelFound = false;
+    bool labelsFound = false;
+
+    for(const QFileInfo &fileInfo : fileInfoList)
+    {
+        QString suffix = fileInfo.suffix().toLower();
+
+        if(suffix == "onnx" || suffix == "pt")
+            modelFound = true;
+
+        if(fileInfo.fileName() == "labels.txt")
+            labelsFound = true;
+
+        if(modelFound && labelsFound)
+            return true;
+    }
+
+    return false;
+}
+
+bool DirTransfer::remoteCopyExists() const
 {
     return m_remoteDir.exists();
 }
 
-bool DirTransfer::transfer(bool overwrite)
+bool DirTransfer::localCopyExists() const
 {
-    if(m_remoteDir.exists())
+    QString selectedDirName = QFileInfo(m_selectedDir.path()).fileName();
+
+    return m_localRepoEntryList.contains(selectedDirName);
+}
+
+bool DirTransfer::transferToRemote(bool overwrite)
+{
+    return transferDir(m_remoteDir, overwrite);
+}
+
+bool DirTransfer::copyToLocal(bool overwrite)
+{
+    QDir dstDir(m_localRepo.filePath(m_selectedDir.dirName()));
+
+    return transferDir(dstDir, overwrite);
+}
+
+QStringList DirTransfer::localRepoEntryList() const
+{
+    return m_localRepoEntryList;
+}
+
+void DirTransfer::onLocalRepoChanged()
+{
+    QStringList newList = m_localRepo.entryList(QDir::Dirs | QDir::NoDotAndDotDot);
+    if (newList == m_localRepoEntryList)
+        return;
+
+    m_localRepoEntryList = newList;
+    emit localRepoEntryListChanged();
+}
+
+bool DirTransfer::transferDir(QDir dst, bool overwrite)
+{
+    if(dst.exists())
     {
         if(!overwrite)
         {
-            qWarning() << "DirTransfer: Failed to transfer, existing remote directory exists: " + m_remoteDir.path();
+            qWarning() << "DirTransfer: Failed to transfer, existing destination directory exists: " + dst.path();
             return false;
         }
         else
         {
-            qDebug() << "DirTransfer: Overwriting existing directory: " + m_remoteDir.path();
+            qDebug() << "DirTransfer: Overwriting existing directory: " + dst.path();
 
-            if(!m_remoteDir.removeRecursively())
+            if(!dst.removeRecursively())
             {
-                qWarning() << "DirTransfer: Failed to overwrite existing remote directory";
+                qWarning() << "DirTransfer: Failed to overwrite existing destination directory " + dst.path();
                 return false;
             }
         }
     }
 
-    if(!QDir().mkpath(m_remoteDir.path()))
+    if(!QDir().mkpath(dst.path()))
     {
-        qWarning() << "DirTransfer: Failed to create remote directory";
+        qWarning() << "DirTransfer: Failed to create destination directory";
         return false;
     }
 
-    QStringList localFiles = m_localDir.entryList(QDir::Files | QDir::NoDotAndDotDot);
+    QStringList internalFiles = m_selectedDir.entryList(QDir::Files | QDir::NoDotAndDotDot);
 
     bool result = true;
 
-    for(const QString &file : localFiles)
+    for(const QString &file : internalFiles)
     {
-        QString src = m_localDir.filePath(file);
-        QString dest = m_remoteDir.filePath(file);
+        QString srcFile = m_selectedDir.filePath(file);
+        QString dstFile = dst.filePath(file);
 
-        if(!QFile::copy(src, dest))
+        if(!QFile::copy(srcFile, dstFile))
             result = false;
     }
 
--- dirtransfer.h
+++ dirtransfer.h
@@ -3,29 +3,47 @@
 
 #include <QObject>
 #include <QDir>
+#include <QFileSystemWatcher>
 #include <QFileInfo>
 #include <QFile>
 #include <QDebug>
 #include "network.h"
 
-#define LOCAL_PARENT_DIR  ".//models"
-#define REMOTE_PARENT_DIR "//srv//models"
+#define LOCAL_REPO "./models"
+#define REMOTE_REPO "models"
 
 class DirTransfer : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QStringList localRepoEntryList READ localRepoEntryList NOTIFY localRepoEntryListChanged FINAL)
+
 public:
     explicit DirTransfer(Network *network, QObject *parent = nullptr);
-    Q_INVOKABLE void setDir(const QDir &dir);
-    Q_INVOKABLE bool remoteCopyExists();
-    Q_INVOKABLE bool transfer(bool overwrite = false);
+    Q_INVOKABLE void setDirToCopy(const QDir &dir);
+    Q_INVOKABLE void setLocalRepoWatcher(bool enable);
+    Q_INVOKABLE bool selectedDirIsValid() const;                //checks for model file and labels.txt file within selected dir
+    Q_INVOKABLE bool remoteCopyExists() const;
+    Q_INVOKABLE bool localCopyExists() const;                   //doesn't make sense when selected dir is in local repo
+    Q_INVOKABLE bool transferToRemote(bool overwrite = false);  //selected dir goes to Jetson
+    Q_INVOKABLE bool copyToLocal(bool overwrite = false);       //Selected dir goes to local repo, doesn't make sense when selected dir is in local repo
+    QStringList localRepoEntryList() const;
 
 signals:
 
+    void localRepoEntryListChanged();
+
+private slots:
+    void onLocalRepoChanged();
+
 private:
+    bool transferDir(QDir dst, bool overwrite);
+
     Network *m_network = nullptr;
-    QDir m_localDir;
+    QDir m_localRepo;
+    QDir m_selectedDir;
     QDir m_remoteDir;
+    QFileSystemWatcher m_fileSystemWatcher;
+    QStringList m_localRepoEntryList;
 };
 
 #endif // DIRTRANSFER_H
