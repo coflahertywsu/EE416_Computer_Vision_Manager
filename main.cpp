0b3e26d2d9e3dce1ffe4d24c2d54a40937fc1847
coflahertywsu <c.oflaherty@wsu.edu>
2026-04-29 22:19:17 -0700

    Added UserProgram object, Instruction bindings, and CvModelConfig bindings

--- main.cpp
+++ main.cpp
@@ -1,47 +1,55 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include <QThread>
-#include <QObject>
 #include <QRemoteObjectNode>
-#include <QSharedPointer>
 #include <QDebug>
 #include "network.h"
 #include "dirtransfer.h"
 #include "gamepadhandler.h"
 #include "videocontroller.h"
-#include "controlworker.h"
+#include "userprogram.h"
+#include "cvmodelconfig.h"
+#include "instruction.h"
 #include "rep_remotevehicle_replica.h"
 #include "rep_remotepipeline_replica.h"
+#include "rep_remotecvmodel_replica.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    // Create the pipeline controller
+    // ------------------------------------------------------------
+    // Local controllers
+    // ------------------------------------------------------------
     VideoController videoController;
-    VideoPipeline *m_videoPipeline = videoController.videoPipeline();
+    VideoPipeline *videoPipeline = videoController.videoPipeline();
 
-    Network *m_network = new Network(&app);
-    GamepadHandler *m_gamepadHandler = new GamepadHandler(&app);
+    Network *network = new Network(&app);
+    GamepadHandler *gamepadHandler = new GamepadHandler(&app);
 
-    QObject::connect(m_network, &Network::initialConnect, m_videoPipeline, &VideoPipeline::onInitialConnect);
-    QObject::connect(m_network, &Network::connectionChanged, m_videoPipeline, &VideoPipeline::onConnectionChanged);
+    QObject::connect(network, &Network::initialConnect, videoPipeline, &VideoPipeline::onInitialConnect);
+    QObject::connect(network, &Network::connectionChanged, videoPipeline, &VideoPipeline::onConnectionChanged);
 
-    QRemoteObjectNode *m_remoteObjectNode = m_network->connectToJetson();
+    // ------------------------------------------------------------
+    // Connect to Jetson control node
+    // ------------------------------------------------------------
+    QRemoteObjectNode *remoteObjectNode = network->connectToJetson();
 
-    if(!m_remoteObjectNode)
+    if (!remoteObjectNode)
     {
-        qWarning() << "ControlWorker: Connect to Jetson failed.";
+        qWarning() << "Main: Connect to Jetson failed.";
         return -1;
     }
 
-    qDebug() << "Conncted to control node";
+    qDebug() << "Connected to control node";
 
-    RemoteVehicleReplica *m_remoteVehicle = m_remoteObjectNode->acquire<RemoteVehicleReplica>("Vehicle");
-    m_remoteVehicle->setParent(&app);
+    // ------------------------------------------------------------
+    // Vehicle replica
+    // ------------------------------------------------------------
+    RemoteVehicleReplica *remoteVehicle = remoteObjectNode->acquire<RemoteVehicleReplica>("Vehicle");
+    remoteVehicle->setParent(&app);
 
-    if(!m_remoteVehicle->waitForSource(2000))
+    if (!remoteVehicle->waitForSource(2000))
     {
         qWarning() << "Vehicle replica not connected to source";
     }
@@ -50,39 +58,190 @@ int main(int argc, char *argv[])
         qDebug() << "Vehicle replica connected to source";
     }
 
-    QObject::connect(m_gamepadHandler, &GamepadHandler::gamepadStatusUpdate, m_remoteVehicle, &RemoteVehicleReplica::onGamepadUpdate);
+    QObject::connect(gamepadHandler, &GamepadHandler::gamepadStatusUpdate, remoteVehicle, &RemoteVehicleReplica::onGamepadUpdate);
 
+    // ------------------------------------------------------------
+    // CvModel replica
+    // ------------------------------------------------------------
+    RemoteCvModelReplica *remoteCvModel = remoteObjectNode->acquire<RemoteCvModelReplica>("CvModel");
 
+    if (!remoteCvModel->waitForSource(2000))
+    {
+        qWarning() << "CvModel replica not connected to source";
+    }
+    else
+    {
+        qDebug() << "CvModel replica connected to source";
+    }
+
+    // ------------------------------------------------------------
+    // Pipeline replica
+    // ------------------------------------------------------------
     QRemoteObjectNode remotePipelineNode;
-    // QRemoteObjectNode remoteControlNode;
 
     if (!remotePipelineNode.connectToNode(QUrl(QStringLiteral("tcp://10.0.0.83:65213"))))
     {
-        qDebug() << "Failed to connect to nodes";
+        qWarning() << "Failed to connect to pipeline node";
         return -1;
     }
 
-    RemotePipelineReplica *remotePipeline = remotePipelineNode.acquire<RemotePipelineReplica>(QStringLiteral("VideoPipeline"));
+    RemotePipelineReplica *remotePipeline =
+        remotePipelineNode.acquire<RemotePipelineReplica>(QStringLiteral("VideoPipeline"));
 
     if (!remotePipeline->waitForSource(3000))
     {
-        qDebug() << "Replicas not connected to source";
-         return -1;
+        qWarning() << "Pipeline replica not connected to source";
+        return -1;
     }
 
-    DirTransfer dirTransfer(m_network);
+    // ------------------------------------------------------------
+    // File transfer
+    // ------------------------------------------------------------
+    DirTransfer dirTransfer(network);
 
-    // Create and load QML engine
+    // ------------------------------------------------------------
+    // User program
+    // ------------------------------------------------------------
+    UserProgram userProgram(network);
+
+    // ------------------------------------------------------------
+    // QML
+    // ------------------------------------------------------------
     QQmlApplicationEngine engine;
 
-    // Pass reference of remoteAppController to QML front end
+    qmlRegisterUncreatableType<CvModelConfig>("App", 1, 0, "CvModelConfig", "Dynamic allocation in UserProgram");
+    qmlRegisterUncreatableType<Instruction>("App", 1, 0, "Instruction", "Dynamic allocation in UserProgram");
+    qmlRegisterUncreatableType<InputCondition>("App", 1, 0, "InputCondition", "Dynamic allocation in UserProgram");
+    qmlRegisterUncreatableType<OutputAction>("App", 1, 0, "OutputAction", "Dynamic allocation in UserProgram");
+    qmlRegisterUncreatableType<InstructionTypes>("App", 1, 0, "InstructionTypes", "Enums only");
+
     engine.rootContext()->setContextProperty("remotePipeline", remotePipeline);
     engine.rootContext()->setContextProperty("videoController", &videoController);
     engine.rootContext()->setContextProperty("dirTransfer", &dirTransfer);
+    engine.rootContext()->setContextProperty("remoteCvModel", remoteCvModel);
+    engine.rootContext()->setContextProperty("userProgram", &userProgram);
 
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
     if (engine.rootObjects().isEmpty())
         return -1;
 
     return app.exec();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <QGuiApplication>
+// #include <QQmlApplicationEngine>
+// #include <QQmlContext>
+// #include <QThread>
+// #include <QObject>
+// #include <QRemoteObjectNode>
+// #include <QSharedPointer>
+// #include <QDebug>
+// #include "network.h"
+// #include "dirtransfer.h"
+// #include "gamepadhandler.h"
+// #include "videocontroller.h"
+// #include "controlworker.h"
+// #include "rep_remotevehicle_replica.h"
+// #include "rep_remotepipeline_replica.h"
+// #include "rep_remotecvmodel_replica.h"
+
+// int main(int argc, char *argv[])
+// {
+//     QGuiApplication app(argc, argv);
+
+//     // Create the pipeline controller
+//     VideoController videoController;
+//     VideoPipeline *m_videoPipeline = videoController.videoPipeline();
+
+//     Network *m_network = new Network(&app);
+//     GamepadHandler *m_gamepadHandler = new GamepadHandler(&app);
+
+//     QObject::connect(m_network, &Network::initialConnect, m_videoPipeline, &VideoPipeline::onInitialConnect);
+//     QObject::connect(m_network, &Network::connectionChanged, m_videoPipeline, &VideoPipeline::onConnectionChanged);
+
+//     QRemoteObjectNode *m_remoteObjectNode = m_network->connectToJetson();
+
+//     if(!m_remoteObjectNode)
+//     {
+//         qWarning() << "ControlWorker: Connect to Jetson failed.";
+//         return -1;
+//     }
+
+//     qDebug() << "Conncted to control node";
+
+//     RemoteVehicleReplica *m_remoteVehicle = m_remoteObjectNode->acquire<RemoteVehicleReplica>("Vehicle");
+//     m_remoteVehicle->setParent(&app);
+
+//     if(!m_remoteVehicle->waitForSource(2000))
+//     {
+//         qWarning() << "Vehicle replica not connected to source";
+//     }
+//     else
+//     {
+//         qDebug() << "Vehicle replica connected to source";
+//     }
+
+//     QObject::connect(m_gamepadHandler, &GamepadHandler::gamepadStatusUpdate, m_remoteVehicle, &RemoteVehicleReplica::onGamepadUpdate);
+
+
+//     QRemoteObjectNode remotePipelineNode;
+//     // QRemoteObjectNode remoteControlNode;
+
+//     if (!remotePipelineNode.connectToNode(QUrl(QStringLiteral("tcp://10.0.0.83:65213"))))
+//     {
+//         qDebug() << "Failed to connect to nodes";
+//         return -1;
+//     }
+
+//     RemotePipelineReplica *remotePipeline = remotePipelineNode.acquire<RemotePipelineReplica>(QStringLiteral("VideoPipeline"));
+
+//     if (!remotePipeline->waitForSource(3000))
+//     {
+//         qDebug() << "Replicas not connected to source";
+//          return -1;
+//     }
+
+//     DirTransfer dirTransfer(m_network);
+
+//     // Create and load QML engine
+//     QQmlApplicationEngine engine;
+
+//     // Pass reference of remoteAppController to QML front end
+//     engine.rootContext()->setContextProperty("remotePipeline", remotePipeline);
+//     engine.rootContext()->setContextProperty("videoController", &videoController);
+//     engine.rootContext()->setContextProperty("dirTransfer", &dirTransfer);
+
+//     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+//     if (engine.rootObjects().isEmpty())
+//         return -1;
+
+//     return app.exec();
+// }
